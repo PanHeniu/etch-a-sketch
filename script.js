5639dfa8c1cca2550a1d720f5c87ad6e53f08f45
PanHeniu <dallaskomputerek@protonmail.com>
2022-07-02 12:10:26 +0200

    Added a hover effect that changes the color of divs, a button that asks the user for the number of squares and functions that remove and rebuild the grid based on the user input. Renamed a few classes and variables and added brief comments.

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 let gridSize = 16;
 let divs = [];
 
+const container = document.getElementById('main-container');
+const button = document.getElementById('btn');
+button.addEventListener('click', function() { // changes the grid size to a value assigned by user
+    while(true) {
+        let newGridSize = parseInt(prompt("Enter the number of squares per side for the new grid"));
+        if (newGridSize <= 100) {
+            removeGrid();
+            buildGrid(newGridSize);
+            colorBoxes();
+            break;
+        }
+    }
+})
+
+// build the grid inside the container div
 function buildGrid(gridSize) {
     for (let i = 0; i < gridSize; i++) {
         divs[i] = document.createElement('div');
-        divs[i].className = `divv`;
+        divs[i].className = `box-container`;
         for (let j = 0; j < gridSize; j++) {
             const box = document.createElement('div');
             box.className = 'box';
@@ -14,4 +29,24 @@ function buildGrid(gridSize) {
     }
 }
 
+// removes the existing grid, so that a new one can be build afterwards
+function removeGrid() {
+    let elementsToRemove = document.getElementsByClassName("box-container");
+    while (elementsToRemove.length > 0) {
+        elementsToRemove[0].parentNode.removeChild(elementsToRemove[0]);
+    }
+}
+
+// changes the color of the hovered squares by adding a 'change' class
+function colorBoxes() {
+    const nodeListBoxes = document.querySelectorAll('.box');
+    for (let i = 0; i < nodeListBoxes.length; i ++) {
+        nodeListBoxes[i].addEventListener('mousemove', function() {
+            nodeListBoxes[i].classList.add('change');
+        })
+    }
+}
+
+// builds the grid with the default value
 buildGrid(gridSize);
+colorBoxes();
